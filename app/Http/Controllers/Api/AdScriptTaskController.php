01d06f469f113c6b69683aa4ba467bc06f5a3be6
Panayot Balkandzhiyski <panayot@despark.com>
2025-11-11 10:50:57 +0200

    Prepare controller and routes

---
+++ app/Http/Controllers/Api/AdScriptTaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreAdScriptTaskRequest;
+use Illuminate\Http\Request;
+
+class AdScriptTaskController extends Controller
+{
+    public function store(StoreAdScriptTaskRequest $request)
+    {
+
+    }
+}
---
+++ app/Http/Requests/Api/StoreAdScriptTaskRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAdScriptTaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'reference_script' => 'required|string',
+            'outcome_description' => 'required|string',
+        ];
+    }
+}
